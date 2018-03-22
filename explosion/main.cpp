504c8db8b2e657163046a68358282b5cffc89ac0
lhrs94 <lhrsohn94@gmail.com>
2018-03-22 11:26:46 -0700

    copy from rects

--- explosion/main.cpp
+++ explosion/main.cpp
@@ -6,10 +6,29 @@
 //  Copyright © 2018 Lynn Hyerin Sohn. All rights reserved.
 //
 
-#include <iostream>
+#include "Sdl.h"
+#include "Input.h"
+#include "Sprite.h"
+#include "Sops.h"
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+int main()
+{
+    Sdl sdl(800, 400);
+    std::deque<Sprite> sprites;
+    while(true)
+    {
+        std::cout << sprites.size() << std::endl;
+        Input input;
+        if(input.done())
+            break;
+        
+        if(input.button == 1)
+            sprites.push_back(Sprite(input.x, input.y));
+        
+        Sops :: update_timeouts(sprites);
+        
+        Sops :: draw(sprites, sdl);
+        
+        Sops :: remove_timeouts(sprites);
+    }
 }
