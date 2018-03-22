638f2f9833a47f42dad90b72e5938345e3e2ff1f
lhrs94 <lhrsohn94@gmail.com>
2018-03-22 11:38:59 -0700

    correct header files

---
+++ explosion/Input.h
@@ -0,0 +1,26 @@
+//
+//  Input.hpp
+//  explosion
+//
+//  Created by Lynn Hyerin Sohn on 2018-03-22.
+//  Copyright © 2018 Lynn Hyerin Sohn. All rights reserved.
+//
+
+#pragma once
+
+#include "Sdl.h"
+
+
+class Input
+{
+public:
+    int x;
+    int y;
+    const uint8_t* key;
+    SDL_Event event;
+    uint32_t button;
+    
+    Input();
+    
+    bool done();
+};
