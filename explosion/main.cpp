a105d14a174c5268055eb7550a4f2c49bfc8c474
lhrs94 <lhrsohn94@gmail.com>
2018-03-21 19:32:34 -0700

    hmm

---
+++ explosion/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  explosion
+//
+//  Created by Lynn Hyerin Sohn on 2018-03-21.
+//  Copyright © 2018 Lynn Hyerin Sohn. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
